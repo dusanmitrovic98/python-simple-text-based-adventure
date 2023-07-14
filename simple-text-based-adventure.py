95a87fb475babfb23621931cff198631ff0e81ce
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-14 22:56:14 +0200

    Update simple-text-based-adventure.py

--- simple-text-based-adventure.py
+++ simple-text-based-adventure.py
@@ -81,3 +81,4 @@ def keep_walking():
     time.sleep(1)
     print("Curiosity gets the better of you, and you step into the portal.")
     time.sleep(1)
+    print("You find yourself in a new dimension, filled with endless possibilities.")
