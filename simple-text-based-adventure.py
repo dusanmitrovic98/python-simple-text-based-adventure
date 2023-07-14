4816ea74fe86a9e93b7e836f9c82c2ebb610fcde
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-14 22:31:52 +0200

    Update simple-text-based-adventure.py

--- simple-text-based-adventure.py
+++ simple-text-based-adventure.py
@@ -4,3 +4,4 @@ def introduction():
     print("Welcome to the Text-Based Adventure Game!")
     time.sleep(1)
     print("You find yourself standing in front of a mysterious door.")
+    time.sleep(1)
