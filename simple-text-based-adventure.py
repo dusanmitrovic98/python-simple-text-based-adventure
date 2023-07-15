1613077fce078f32ba0432e36ce8bfae5b8a94f2
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-07-15 21:11:45 +0200

    Update simple-text-based-adventure.py

--- simple-text-based-adventure.py
+++ simple-text-based-adventure.py
@@ -109,3 +109,4 @@ def dark_cave():
     print("You bravely enter the dark cave and hear a menacing growl.")
     time.sleep(1)
     print("You face a fearsome dragon and engage in an epic battle.")
+    time.sleep(1)
